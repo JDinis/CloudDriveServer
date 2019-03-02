d6ca8e84bb54bbaa9df72b7b50e37c6776cc03dd
João Dinis <j.p.dinis89@gmail.com>
2019-03-02 08:44:48 +0000

    Versao quase final

--- server.js
+++ server.js
@@ -4,21 +4,32 @@ config = require('./Scripts/config'),
 passport  = require('./Scripts/expressDados').passport,
 db = require('./Scripts/db'),
 fs = require('fs'),
-path = require('path'),
-cookieParser  = require('cookie-parser'),
-location = require('location-href')
+path = require('path');
 var session;
-location()
-                                                                            
+
 function isLoggedIn(req, res, next) {                                       // Função que permite garantir que 
                                                                             // o utilizador esta autenticado.
-    
+    var logOut = 
+    (req, res) => {  
+        if(req.session!==undefined){
+        req.session.destroy(function(err) {
+            if(err) {
+                console.log(err);
+                res.json(JSON.stringify({success:false}));
+            } else {
+                req.logout();
+                res.json(JSON.stringify({success:false}));
+            }
+      })}else{
+            req.logout();
+            res.json(JSON.stringify({success:false}));
+      }};
+
         if(session!==undefined){
             if(session._id!==undefined){
                 passport.deserializeUser(session._id,(err,user)=>{
                     if(err){
-                        location.set('/logout');
-                        res.end();
+                        logOut(req,res);
                     }
             
                     req.user = user;
@@ -27,11 +38,10 @@ function isLoggedIn(req, res, next) {                                       // F
                         return next();                                                      // proxima função
                 });
             }else{
-                res.end('<script>var hi = function changeUrl(){window.location["href"]="http://localhost:3001/logout"}</script>javascript:hi();');
+                logOut(req,res);
             }
         }else{
-            location.set('/logout');
-            res.end();
+            logOut(req,res);
         }
 };
 
@@ -65,11 +75,40 @@ app.post('/login', passport.authenticate('local-login'),function(req,res){
 });
 
 
-app.post('/signup',
-    passport.authenticate('local-signup'));
+app.post('/Signup',
+    passport.authenticate('local-signup'),function(req,res){
+        req.logIn(req.user,function(err){
+            if(err!==undefined){
+                if(err!==null){
+                    console.log(err);
+                }
+            }
+    
+            if(req.session.passport.user!==undefined){
+                if(session===undefined)
+                    session=req.session;
+                session._id=req.session.passport.user;
+                session.save();
+            }
+            res.json(JSON.stringify({User:req.user}));
+        });
+    });
 
 
-app.post('/users/adduser',isLoggedIn,function(req,res){
+    app.post('/users/adduser',isLoggedIn,function(req,res){
+        var profile = {};
+        profile.username = req.body.username;
+        profile.password = req.body.password;
+        profile.email = req.body.email;
+        profile.firstName = req.body.firstName;
+        profile.lastName = req.body.lastName;
+        profile.picurl = req.body.picurl;
+        profile.admin = req.body.admin;
+        db.addUser(profile);
+        res.redirect('/users');
+    });
+
+app.put('/users/edituser/:userid',isLoggedIn,function(req,res){
     var profile = {};
     profile.username = req.body.username;
     profile.password = req.body.password;
@@ -78,22 +117,97 @@ app.post('/users/adduser',isLoggedIn,function(req,res){
     profile.lastName = req.body.lastName;
     profile.picurl = req.body.picurl;
     profile.admin = req.body.admin;
-    db.addUser(profile);
+    db.updateUser(req.userid,profile);
     res.redirect('/users');
+});
+
+app.get('/users/list',(req,res)=>{
+    db.getAllUsers((err,users)=>{
+        if (err)
+        console.log('err', err)
+
+        res.json(JSON.stringify(users));
+    })
+});
+
+app.get('/users/:username',isLoggedIn,function(req,res){
+    var profile = {};
+    profile.username = req.body.username;
+    profile.password = req.body.password;
+    profile.email = req.body.email;
+    profile.firstName = req.body.firstName;
+    profile.lastName = req.body.lastName;
+    profile.picurl = req.body.picurl;
+    profile.admin = req.body.admin;
+    
+    db.findUser(profile.username,(err,user)=>{
+        if(err)
+            console.log(err)
+    
+        res.json(JSON.stringify(user));
+    });
+
+    
+});
+
+app.get('/backend/profile',isLoggedIn,(req,res)=>{
+    res.json(req.user);
+});
+
+app.get('/backend/profile/:username',isLoggedIn,(req,res)=>{
+    db.findUser(req.username,(err,user)=>{
+        res.json(req.user);
+    })
+});
+
+app.get('/backend/file/list',isLoggedIn,(req,res)=>{
+    fs.exists("uploads/"+req.user.username,(exists)=>{
+        if(!exists)
+            fs.mkdir("uploads/"+req.user.username+"/",(err)=>{ 
+                if(err){
+                    console.log(err);
+                }
+        });
+        else {
+            fs.readdir("uploads/"+req.user.username,(err,files)=>{
+                if(err)
+                    console.log(err);
+                
+                
+                res.json(JSON.stringify({files:files,success:true}));
+            });
+        }
+    });
+});
+
+app.get('/backend/file/list/:username',isLoggedIn,(req,res)=>{
+    fs.exists("uploads/"+req.username,(exists)=>{
+        if(!exists)
+            fs.mkdir("uploads/"+req.username+"/",(err)=>{ 
+                if(err){
+                    console.log(err);
+                }
+        });
+        else {
+            fs.readdir("uploads/"+req.username,(err,files)=>{
+                if(err)
+                    console.log(err);
+                
+                
+                res.json(JSON.stringify({files:files,success:true}));
+            });
+        }
+    });
 });
 
 app.post("/backend/file/upload",isLoggedIn,(req,res)=>{
     var form = new multiparty.Form();
     form.parse(req,(err,fields,files)=>{
+        var count = 0;
         files.files.forEach(file=>{
-            fs.exists("uploads/",(exists)=>{
+            fs.exists("uploads/"+req.user.username+"/"+path.relative(path.dirname(file.originalFilename),__dirname),(exists)=>{
                 if(!exists)
-                fs.mkdir("uploads/",(err)=>{ });
-            });
-
-            fs.exists("uploads/"+req.user.username+"/",(exists)=>{
-                if(!exists)
-                    fs.mkdir("uploads/"+req.user.username+"/",(err)=>{ 
+                    fs.mkdir("uploads/"+req.user.username+"/"+path.dirname(file.originalFilename),{recursive:true},(err)=>{ 
                         if(err){
                             console.log(err);
                         }
@@ -104,10 +218,101 @@ app.post("/backend/file/upload",isLoggedIn,(req,res)=>{
                         console.log(err);
                     }
                     
+                    if(count===files.length-1)
+                        res.json(JSON.stringify({success:true}))
                     fs.unlink(file.path,()=>{});
+                    count++;
                 });
             });
         });
+    });
+});
+
+app.post("/backend/file/upload/:username",isLoggedIn,(req,res)=>{
+    var form = new multiparty.Form();
+    form.parse(req,(err,fields,files)=>{
+        var count = 0;
+        files.files.forEach(file=>{
+            fs.exists("uploads/"+req.username+"/"+path.relative(path.dirname(file.originalFilename),__dirname),(exists)=>{
+                if(!exists)
+                    fs.mkdir("uploads/"+req.username+"/"+path.dirname(file.originalFilename),{recursive:true},(err)=>{ 
+                        if(err){
+                            console.log(err);
+                        }
+                })
+                        
+                fs.copyFile(file.path,`uploads/${req.user.username}/${file.originalFilename}`,(err)=>{
+                    if(err){
+                        console.log(err);
+                    }
+                    
+                    if(count===files.length-1)
+                        res.json(JSON.stringify({success:true}))
+                    fs.unlink(file.path,()=>{});
+                    count++;
+                });
+            });
+        });
+    });
+});
+
+app.get("/backend/file/:filename",isLoggedIn,(req,res)=>{
+    fs.exists("uploads/"+req.user.username,(exists)=>{
+        if(!exists)
+            fs.mkdir("uploads/"+req.user.username+"/",(err)=>{ 
+                if(err){
+                    console.log(err);
+                }
+                res.json(JSON.stringify({success:false}))
+        });
+        else {
+            res.sendFile("uploads/"+req.user.username+'/'+req.filename);
+        }
+    });
+});
+
+app.get("/backend/file/:username/:filename",isLoggedIn,(req,res)=>{
+    fs.exists("uploads/"+req.username,(exists)=>{
+        if(!exists)
+            fs.mkdir("uploads/"+req.username+"/",(err)=>{ 
+                if(err){
+                    console.log(err);
+                }
+                res.json(JSON.stringify({success:false}))
+        });
+        else {
+            res.sendFile("uploads/"+req.username+'/'+req.filename);
+        }
+    });
+});
+
+app.delete("/backend/file/:username/:filename",isLoggedIn,(req,res)=>{
+    fs.exists("uploads/"+req.username,(exists)=>{
+        if(!exists)
+            fs.mkdir("uploads/"+req.username+"/",(err)=>{ 
+                if(err){
+                    console.log(err);
+                }
+                res.json(JSON.stringify({success:false}))
+        });
+        else {
+            res.sendFile("uploads/"+req.username+'/'+req.filename);
+        }
+    });
+});
+
+app.delete("/backend/file/:filename",isLoggedIn,(req,res)=>{
+    fs.exists("uploads/"+req.user.username,(exists)=>{
+        if(!exists)
+            fs.mkdir("uploads/"+req.user.username+"/",(err)=>{ 
+                if(err){
+                    console.log(err);
+                }
+                res.json(JSON.stringify({success:false}))
+        });
+        else {
+            fs.unlink("uploads/"+req.user.username+'/'+req.filename,()=>{});
+        }
     });
 });
 
